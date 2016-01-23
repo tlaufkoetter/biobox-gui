d06c84d2a033bfa79d566bfc9128a560835d1ca7
Tobias Laufkötter <tlaufkoetter@techfak.uni-bielefeld.de>
2016-01-23 19:10:48 +0100

    attempt to validate with json schema.
    no error handling so far.

--- src/app/bioboxes.py
+++ src/app/bioboxes.py
@@ -1,38 +1,10 @@
 import requests
 import yaml
+from jsonschema import validate
+import json
 
 
-class Yaml():
-    """
-    is the parent of every class that can be generated from yaml files.
-    """
-    MUST_HAVE = []
-
-    def __init__(self, yaml_dict):
-        """
-        Forms an object from the given dictionary.
-
-        :param yaml_dict: a dictionary as it is parsed from yaml code.
-        :return: Yaml base
-        """
-        self.validate_yaml(yaml_dict)
-
-    def validate_yaml(self, yaml_dict):
-        """
-        validates the given dictionary against the MUST_HAVE keys.
-
-        :param yaml_dict: a dictionary as it is parsed from yaml code.
-        :return: None
-        :raises: LookupError when key requirements aren't fulfilled.
-        """
-        for key in self.MUST_HAVE:
-            if yaml_dict.has_key(key) and yaml_dict.get(key):
-                pass
-            else:
-                raise LookupError, 'error with the "%s" key' % key
-
-
-class Biobox(Yaml):
+class Biobox():
     """
     represents a generic biobox container description.
     """
@@ -44,25 +16,21 @@ class Biobox(Yaml):
     KEY_TASKS = 'tasks'
     KEY_TITLE = 'title'
 
-    MUST_HAVE = [
-        KEY_IMAGE,
-        KEY_ID,
-        KEY_DESCRIPTION,
-        KEY_TASKS,
-        KEY_TITLE
-    ]
-
     def __init__(self, yaml_dict):
         """
         Forms an object from the given dictionary.
 
         :param interface: the interface of the biobox.
         :param yaml_dict: a dictionary as it is parsed from yaml code.
-        :return: Yaml base
+        :return:  base
         """
-        Yaml.__init__(self, yaml_dict)
+        with open('app/static/image_schema.json', 'r') as schema_file:
+            schema_string = schema_file.read()
+            schema = json.loads(schema_string)
+            validate(yaml_dict, schema)
         self.image = self.Image(yaml_dict.get(self.KEY_IMAGE))
-        self.tasks = [self.Task(task) for task in yaml_dict.get(self.KEY_TASKS)]
+        self.tasks = [self.Task(task)
+                      for task in yaml_dict.get(self.KEY_TASKS)]
         self.pmid = yaml_dict.get(self.KEY_ID)
         self.homepage = yaml_dict.get(self.KEY_HOME_PAGE)
         self.mailing_list = yaml_dict.get(self.KEY_MAILING_LIST)
@@ -75,7 +43,7 @@ class Biobox(Yaml):
         dictionary[self.KEY_TASKS] = [task.__dict__ for task in self.tasks]
         return dictionary
 
-    class Image(Yaml):
+    class Image():
         """
         is a helper class for :class:`Biobox`.
         """
@@ -83,40 +51,31 @@ class Biobox(Yaml):
         KEY_REPO_URL = 'repo'
         KEY_SRC_URL = 'source'
 
-        MUST_HAVE = [
-            KEY_CONTAINER_URI,
-            KEY_REPO_URL
-        ]
-
         def __init__(self, yaml_dict):
             """
             Forms an object from the given dictionary.
 
             :param yaml_dict: a dictionary as it is parsed from yaml code.
-            :return: Yaml base
+            :return:  base
             """
-            Yaml.__init__(self, yaml_dict)
             self.dockerhub = yaml_dict.get(self.KEY_CONTAINER_URI)
             self.repo = yaml_dict.get(self.KEY_REPO_URL)
             self.source = yaml_dict.get(self.KEY_SRC_URL)
 
-    class Task(Yaml):
+    class Task():
         KEY_NAME = 'name'
         KEY_INTERFACE = 'interface'
 
-        MUST_HAVE = [
-            KEY_NAME,
-            KEY_INTERFACE
-        ]
-
         def __init__(self, yaml_dict):
-            Yaml.__init__(self, yaml_dict)
             self.name = yaml_dict.get(self.KEY_NAME)
             self.interface = yaml_dict.get(self.KEY_INTERFACE)
 
 
 def get_current_bioboxes():
-    response = requests.get('https://raw.githubusercontent.com/pbelmann/data/feature/new-image-list/images.yml')
+    response = requests.get(
+        'https://raw.githubusercontent.com'
+        '/pbelmann/data/feature/new-image-list/images.yml'
+    )
     return from_list(yaml.load(response.text))
 
 
