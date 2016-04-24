05d5d8f10a1ef4bcc6386a7fa93865219c91d2ab
Tobias Laufkötter <tlaufkoetter@techfak.uni-bielefeld.de>
2016-04-24 21:09:33 +0200

    update task api.

--- bioboxgui/resources/tasks.py
+++ bioboxgui/resources/tasks.py
@@ -1,18 +1,20 @@
 import json
 
 import requests
-from flask import abort, request
+from flask import abort
 from flask_restful import marshal, Resource, fields, reqparse
 
+JOB_PROXY_IP = 'http://localhost:9999/v1/jobproxy'
+
 simple_task = {
-    "id": fields.String
+    'id': fields.String
 }
 
 full_task = {
-    "description": fields.String,
-    "id": fields.String,
-    "stderr": fields.String,
-    "stdout": fields.String
+    'description': fields.String,
+    'id': fields.String,
+    'stderr': fields.String,
+    'stdout': fields.String
 }
 
 
@@ -23,38 +25,35 @@ class TasksAll(Resource):
             'user', type=str, required=True, location='json'
         )
         self.reqparse.add_argument(
-            'cores', type=int, required=False, location='json'
+            'container', type=str, required=True, location='json'
         )
         self.reqparse.add_argument(
-            'memory', type=int, required=False, location='json'
-        )
-        self.reqparse.add_argument(
-            'cputime', type=int, required=False, location='json'
-        )
-        self.reqparse.add_argument(
-            'cmd', type=str, required=False, location='json'
+            'cmd', type=str, required=True, location='json'
         )
 
     def post(self):
-        """
+        '''
         creates a new task.
         :return: string of the task id
-        """
-        s = self.reqparse.parse_args()
-        response = requests.post('http://localhost:9999/v1/jobproxy/submit', json=s)
+        '''
+        try:
+            s = self.reqparse.parse_args()
+        except:
+            abort(400)
+        response = requests.post(JOB_PROXY_IP + '/submit', json=s)
         if response.status_code == 200 and response.content:
-            return marshal({"id": response.content.decode("utf-8")}, simple_task), 201
+            return marshal({'id': response.content.decode('utf-8')}, simple_task), 201
         else:
             abort(502)
 
     def get(self):
-        """
+        '''
         queries all the tasks.
         :return: json formatted list of tasks.
-        """
-        response = requests.get("http://localhost:9999/v1/jobproxy/state")
+        '''
+        response = requests.get(JOB_PROXY_IP + '/state')
         if response.status_code == 200 and response.content:
-            states = json.loads(response.content.decode("utf-8"))['state']
+            states = json.loads(response.content.decode('utf-8'))['state']
             return marshal(states, full_task, envelope='states')
         else:
             abort(502)
@@ -62,21 +61,21 @@ class TasksAll(Resource):
 
 class TaskId(Resource):
     def get(self, task_id):
-        """
+        '''
         queries the state of given task.
         :param task_id: the id of the task to query.
         :return: json formatted task state
-        """
-        response = request.post("http://localhost:9999/v1/jobproxy/state", task_id)
+        '''
+        response = requests.post(JOB_PROXY_IP + '/state', task_id)
         if response.status_code == 200 and response.content:
             state = json.loads(response.content.decode('utf-8'))['state']
 
     def delete(self, task_id):
-        """
+        '''
         deletes a specific task.
         :param task_id: the task's id
         :return: none
-        """
+        '''
         abort(502)
 
 
