159ac45d401c0f4a75dc19c38d486434815781e6
Tobias Laufkötter <tlaufkoetter@techfak.uni-bielefeld.de>
2016-09-03 14:10:59 +0200

    Adds notification support.

--- bioboxgui/static/angular/app.module.js
+++ bioboxgui/static/angular/app.module.js
@@ -2,5 +2,18 @@
     'use strict';
 
     angular
-        .module('BioboxGui', ['ngRoute', 'angular-storage']);
+        .module('BioboxGui', ['ngRoute', 'angular-storage', 'ui-notification'])
+        .config(configure);
+
+    function configure(NotificationProvider) {
+        NotificationProvider.setOptions({
+            delay: 10000,
+            startTop: 20,
+            startRight: 10,
+            verticalSpacing: 20,
+            horizontalSpacing: 20,
+            positionX: 'right',
+            positionY: 'top'
+        });
+    }
 })();
